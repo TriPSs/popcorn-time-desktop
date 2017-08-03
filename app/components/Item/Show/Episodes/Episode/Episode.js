268deee25d4c6ae1d0c9ad481bb8ce5514a97d19
Tycho Bokdam <t.bokdam@seronaris.nl>
2017-08-03 19:59:21 +0200

    Episode component [ski-ci]

--- app/components/Item/Show/Episodes/Episode/Episode.js
+++ app/components/Item/Show/Episodes/Episode/Episode.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import classNames from 'classnames'
 
+import { getHighestQualtity } from 'api/Torrents/TorrentsHelpers'
 import Watched from 'components/Watched'
 import placeHolderImage from 'images/posterholder.png'
-import type { Props } from './EpisodesTypes'
+import type { Props, State } from './EpisodeTypes'
 
 export default class extends React.Component {
 
   props: Props
+
+  state: State = {
+    torrent: null,
+  }
 
   constructor(props) {
     super(props)
@@ -19,28 +24,21 @@ export default class extends React.Component {
 
   tomorrow: number
 
-  activeEpisodeComponent
+  componentDidMount() {
+    const { episode: { torrents } } = this.props
 
-  componentWillReceiveProps(nextProps) {
-    const { episodesListComponent: oldEpisodesListComponent } = this.props
-    const { episodesListComponent: newEpisodesListComponent } = nextProps
-
-    if (!oldEpisodesListComponent && newEpisodesListComponent) {
-      newEpisodesListComponent.scrollLeft = (
-        this.activeEpisodeComponent.offsetLeft - this.activeEpisodeComponent.offsetWidth
-      )
-    }
+    this.setState({
+      torrent: getHighestQualtity(torrents)
+    })
   }
 
-  componentDidUpdate(prevProps) {
-    const { selectedSeason: newSelectedSeason } = this.props
-    const { selectedSeason: oldSelectedSeason } = prevProps
-    const { episodesListComponent }             = this.props
+  selectTorrent = (quality) => {
+    const { episode: { torrents } } = this.props
 
-    if (newSelectedSeason !== oldSelectedSeason && episodesListComponent) {
-      episodesListComponent.scrollLeft = (
-        this.activeEpisodeComponent.offsetLeft - this.activeEpisodeComponent.offsetWidth
-      )
+    if (torrents[quality]) {
+      this.setState({
+        torrent: torrents[quality],
+      })
     }
   }
 
@@ -51,84 +49,86 @@ export default class extends React.Component {
   }
 
   render() {
-    const { selectedSeason, selectedEpisode } = this.props
-    const { selectSeasonAndEpisode }          = this.props
+    const { episode, setSelectedEpisodeRef }          = this.props
+    const { selectSeasonAndEpisode, selectedEpisode } = this.props
+
+    const { torrent } = this.state
 
     return (
-      <div className={'list'}>
-        {selectedSeason.episodes.map(episode => (
-          <a
-            ref={(ref) => {
-              if (episode.number === selectedEpisode.number) {
-                this.activeEpisodeComponent = ref
-              }
-            }}
-            className={classNames('list__item--hor',
-              {
-                'list__item--active' : episode.number === selectedEpisode.number,
-                'list__item--watched': episode.watched && episode.number !== selectedEpisode.number,
-              }, episode.aired > this.tomorrow
-                ? 'list__item--disabled'
-                : 'list__item--available',
+      <a
+        ref={(ref) => {
+          if (episode.number === selectedEpisode.number) {
+            setSelectedEpisodeRef(ref)
+          }
+        }}
+        className={classNames('list__item--hor',
+          {
+            'list__item--active' : episode.number === selectedEpisode.number,
+            'list__item--watched': episode.watched && episode.number !== selectedEpisode.number,
+          }, episode.aired > this.tomorrow
+            ? 'list__item--disabled'
+            : 'list__item--available',
+        )}
+        key={episode.number}
+        onClick={() => episode.aired < this.tomorrow
+          ? selectSeasonAndEpisode(selectedEpisode.season, episode.number)
+          : null}
+      >
+        <div className={'list__item-image-container--hor'}>
+          <img
+            className={classNames(
+              'animated fadeIn',
+              'list__item-image',
+              { 'list__item-image-placeholder': !episode.images },
             )}
-            key={episode.number}
-            onClick={() => episode.aired < this.tomorrow
-              ? selectSeasonAndEpisode(selectedSeason.number, episode.number)
-              : null}
-          >
-            <div className={'list__item-image-container--hor'}>
-              <img
+            src={episode.images ? episode.images.poster.thumb : placeHolderImage}
+            alt={episode.title} />
+
+          <div className={'list__item-overlay'} />
+
+          {episode.aired > this.tomorrow && (
+            <div className={'list__item-overlay-text'}>
+              {this.getFormattedAired(episode.aired)}
+            </div>
+          )}
+
+          {episode.aired < this.tomorrow && (
+            <Watched
+              className={'list__item-watched'}
+              item={episode} />
+          )}
+
+          <div className={'list__item-play'}>
+            <i className={'ion-ios-play'} />
+          </div>
+
+          <div className={'list__item-qualities'}>
+            {Object.keys(episode.torrents).map(quality => (
+              <div
+                key={quality}
                 className={classNames(
-                  'animated fadeIn',
-                  'list__item-image',
-                  { 'list__item-image-placeholder': !episode.images },
+                  'list__item-quality',
+                  {
+                    'list__item-quality--active'   : torrent && torrent.quality === quality,
+                    'list__item-quality--available': episode.torrents[quality],
+                  },
                 )}
-                src={episode.images ? episode.images.poster.thumb : placeHolderImage}
-                alt={episode.title} />
-
-              <div className={'list__item-overlay'} />
-
-              {episode.aired > this.tomorrow && (
-                <div className={'list__item-overlay-text'}>
-                  {this.getFormattedAired(episode.aired)}
-                </div>
-              )}
-
-              {episode.aired < this.tomorrow && (
-                <Watched
-                  className={'list__item-watched'}
-                  item={episode} />
-              )}
-
-              <div className={'list__item-play'}>
-                <i className={'ion-ios-play'} />
+                onClick={() => this.selectTorrent(quality)}>
+                {quality}
               </div>
+            ))}
+          </div>
 
-              <div className={'list__item-qualities'}>
-                {Object.keys(episode.torrents).map(quality => (
-                  <div
-                    key={quality}
-                    className={classNames(
-                      'list__item-quality',
-                      { 'list__item-quality--active': quality === '480p' }
-                    )}>
-                    {quality}
-                  </div>
-                ))}
-              </div>
+        </div>
 
-            </div>
+        <div className={'list__item-title'}>
+          {episode.title ? episode.title : `Episode ${episode.number}`}
+        </div>
 
-            <div className={'list__item-title'}>
-              {episode.title ? episode.title : `Episode ${episode.number}`}
-            </div>
-
-            <div className={'list__item-summary'}>
-              {episode.summary}
-            </div>
-          </a>
-        ))}
-      </div>
+        <div className={'list__item-summary'}>
+          {episode.summary}
+        </div>
+      </a>
     )
   }
 }
